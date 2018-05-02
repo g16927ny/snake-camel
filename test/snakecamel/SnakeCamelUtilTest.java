f92be443e8c8477ebf3802aac649bb923adc6d50
g16927ny <g16927ny@gm.tsuda.ac.jp>
2018-05-02 16:26:21 +0900

    テストケースを作成した

---
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -0,0 +1,15 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SnakeCamelUtilTest {
+
+	@Test
+	public void multiplyで乗算結果が取得できる(){
+		fail("まだ実装されていません");
+	}
+
+}
